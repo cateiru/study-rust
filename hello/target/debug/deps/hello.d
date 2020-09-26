b46b071e6be4704498ed43939eb67e45c610603c
Yuto Watanabe <yuto.w51942@gmail.com>
2020-09-27 00:39:11 +0900

    remove target file

--- hello/target/debug/deps/hello.d
+++
@@ -1,5 +0,0 @@
-/Users/yuto_w/Git/study-rust/hello/target/debug/deps/hello: src/main.rs
-
-/Users/yuto_w/Git/study-rust/hello/target/debug/deps/hello.d: src/main.rs
-
-src/main.rs:
--- hello/target/debug/hello.d
+++
@@ -1,1 +0,0 @@
-/Users/yuto_w/Git/study-rust/hello/target/debug/hello: /Users/yuto_w/Git/study-rust/hello/src/main.rs
