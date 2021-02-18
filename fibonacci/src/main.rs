64acd11182f0775bba9a384890830e9d4a34931f
Yuto Watanabe <yuto.w51942@gmail.com>
2021-02-18 18:30:10 +0900

    fibonacci作った

--- fibonacci/src/main.rs
+++ fibonacci/src/main.rs
@@ -1,14 +1,36 @@
-fn fibonacci(a: i128) -> i128 {
-    if a == 0 {
-        return 0;
-    } else if a == 1 {
-        return 1;
-    }
-    fibonacci(a - 1) + fibonacci(a - 2)
+// https://www.christopherbiscardi.com/why-can-t-i-early-return-in-an-if-statement-in-rust
+
+// TYPE: 1
+// fn fibonacci(a: u128) -> u128 {
+//     if a <= 1 {
+//         a
+//     } else {
+//         fibonacci(a - 1) + fibonacci(a - 2)
+//     }
+// }
+
+// TYPE: 2
+// fn fibonacci(a: u128) -> u128 {
+//     match a {
+//         0 => 0,
+//         1 => 1,
+//         _ => fibonacci(a - 1) + fibonacci(a - 2),
+//     }
+// }
+
+fn fibonacci(a: u128) -> u128 {
+    println!("{}", a);
+    if a == 10 {
+        1
+    } else {
+        0
+    };
+
+    0
 }
 
 fn main() {
-    let num: i128 = 100;
+    let num: u128 = 10;
 
     println!("ans: {}", fibonacci(num));
 }
