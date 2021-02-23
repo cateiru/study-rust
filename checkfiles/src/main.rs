2da946fc3243b22168c3581d7b804e3d549a0afc
Yuto Watanabe <yuto.w51942@gmail.com>
2021-02-23 17:57:02 +0900

    yatta

--- checkfiles/src/main.rs
+++ checkfiles/src/main.rs
@@ -1,10 +1,10 @@
-use std::{fs, path::Path, process};
+use std::{error::Error, fs, io, path::Path, process};
 
 mod cli;
 
 use crate::cli::Cli;
 
-fn main() {
+fn main() -> Result<(), Box<dyn Error>> {
     let cli = Cli::new();
 
     let target: &Path = cli.target();
@@ -14,9 +14,52 @@ fn main() {
     //     _ => process::exit(1),
     // }
 
-    let a = fs::read_dir(target);
+    // for path in fs::read_dir(target)? {
+    //     let entry = path?;
+    //     let path = entry.path();
 
-    for e in a {
-        println!("{:?}", e);
+    //     if path.is_dir() {
+    //         println!("{:?} is directory.", path);
+    //     } else {
+    //         println!("{:?} is file.", path);
+    //     }
+    // }
+
+    files(target, "".to_string())?;
+
+    Ok(())
+}
+
+fn files(dir: &Path, indent: String) -> io::Result<()> {
+    if dir.is_dir() {
+        let paths: Vec<_> = fs::read_dir(dir)
+            .unwrap()
+            .map(|res| res.unwrap().path())
+            .collect();
+
+        let all_files = paths.len();
+        let mut new_indent;
+
+        for (index, path) in paths.iter().enumerate() {
+            if path.is_dir() {
+                if all_files - 1 == index {
+                    println!("{}└─ {:?}", indent, path);
+                    new_indent = format!("{}  ", indent);
+                } else {
+                    println!("{}├─ {:?}", indent, path);
+                    new_indent = format!("{}│ ", indent);
+                }
+
+                files(&path, new_indent)?;
+            } else {
+                if all_files - 1 == index {
+                    println!("{}└─ {:?}", indent, path);
+                } else {
+                    println!("{}├─ {:?}", indent, path);
+                }
+            }
+        }
     }
+
+    Ok(())
 }
