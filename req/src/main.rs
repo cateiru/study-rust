81cb70fa579a121343f473096a30b806467655b7
Yuto Watanabe <yuto.w51942@gmail.com>
2021-02-25 13:06:54 +0900

    added request project

--- req/src/main.rs
+++ req/src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
-    println!("Hello, world!");
+use reqwest::get;
+
+use std::error::Error;
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let body = get("https://www.rust-lang.org").await?.text().await?;
+
+    println!("body = {:?}", body);
+
+    Ok(())
 }
